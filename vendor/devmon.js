14577a172b77fc03936d6bcc4c4ecb0d1c07e8a0
Karan Sikka <kssworld93@gmail.com>
2014-02-26 00:49:54 -0500

    untested node-inspector and forever integration

--- vendor/devmon.js
+++ vendor/devmon.js
@@ -5,32 +5,44 @@ var child_process = require('child_process');
 var http = require('http');
 var formidable = require('formidable');
 
+var forever = require('forever-monitor');
+
 var DEBUG = true;
 
 var devmon_log = function(s) {
     console.log('[Appcubator] ' + s);
 };
 
-var spawnApp = function () {
-    var child_app = child_process.spawn(app_cmd, app_args, {env: process.env});
+var spawnApp = function (command) {
+    /* command should be an array of args */
+    forever.start(command, {watch:true});
 
-    child_app.stdout.on('data', function (data) {
-      process.stdout.write(data);
+    forever.on('start', function () {
+        devmon_log('App has started');
+    }).on('exit', function () {
+        devmon_log('App has quit');
+    }).on('restart', function () {
+        devmon_log('App has restarted');
     });
-
-    child_app.stderr.on('data', function (data) {
-      process.stderr.write(data);
-    });
-
-    child_app.on('close', function (code) {
-      devmon_log('Child process exited with code ' + code);
-    });
-
-    return child_app;
 };
 
+var spawnNodeInspector = function() {
+    /* note that this will only work if the app has node --debug. */
+    var child = forever.start([ 'node-inspector']);
+    forever.on('start', function () {
+        devmon_log('Node-inspector has started');
+    }).on('exit', function () {
+        devmon_log('Node-inspector has quit');
+    }).on('restart', function () {
+        devmon_log('Node-inspector has restarted');
+    });
+};
+
+var updatingCode = false;
 var updateCode = function (tarpath, callback) {
+    updatingCode = true;
     child_process.exec('tar -xvf '+tarpath, function(err, stdout, stderr) {
+        updatingCode = false;
         if (err) devmon_log(err);
         else {
             devmon_log('Code updated. Tar output:');
@@ -50,72 +62,94 @@ var httpProxy = function (LOCAL_PORT, REMOTE_ADDR, REMOTE_PORT) {
      *   http://www.catonmat.net/http-proxy-in-nodejs/
      *
      */
-    var updatingCode = false;
     var s = http.createServer(function(request, response) {
         var ip = request.connection.remoteAddress;
         devmon_log(ip + ": " + request.method + " " + request.url);
-        if (updatingCode) {
-            response.writeHead(503, {'content-type': 'text/plain'});
-            response.end('Updating code!');
-        } else {
-            if (request.url.indexOf('__update_code__') != -1) {
-                updatingCode = true;
-                var form = new formidable.IncomingForm();
+        /* [ ROUTE ] update code */
+        if (!updatingCode && request.url.indexOf('__update_code__') != -1) {
+            var form = new formidable.IncomingForm();
 
-                form.parse(request, function(err, fields, files) {
-                    // Note: it will write this relative to the current working directory which should be appdir
-                    if (err) {
-                        updatingCode = false;
-                        devmon_log(err);
+            form.parse(request, function(err, fields, files) {
+                // Note: it will write this relative to the current working directory which should be appdir
+                if (err) {
+                    devmon_log(err);
+                } else {
+                    devmon_log('Written out to '+files.code.path);
+                    updateCode(files.code.path, function(){
+                        response.writeHead(200, {'content-type': 'text/plain'});
+                        response.end('OK');
+                    });
+                }
+            });
+        /* [ ROUTE ] node-inspector */
+        } else if (request.url.indexOf('/dev/node-inspector/') != -1) {
+            var proxy_request = http.request({method:request.method,
+                                              hostname: '127.0.0.1',
+                                              port: 8080,
+                                              path: request.url.replace('/dev/node-inspector/', ''),
+                                              headers: request.headers});
+            proxy_request.addListener('response', function(proxy_response) {
+                proxy_response.addListener('data', function(chunk) {
+                    response.write(chunk, 'binary');
+                });
+                proxy_response.addListener('end', function() {
+                    response.end();
+                });
+                response.writeHead(proxy_response.statusCode, proxy_response.headers);
+            });
+            request.addListener('data', function(chunk) {
+                proxy_request.write(chunk, 'binary');
+            });
+            proxy_request.addListener('error', function(err) {
+                if (err.code == 'ECONNREFUSED') {
+                    response.writeHead(502, {'content-type': 'text/plain'});
+                    // TODO attempt to bring it back up
+                    response.end('node-inspector is down.'); 
+                }
+            });
+            request.addListener('end', function() {
+                proxy_request.end();
+            });
+        /* [ ROUTE ] ping */
+        } else if (request.url.indexOf('__ping__') != -1) {
+            response.writeHead(200, {'content-type': 'text/plain'});
+            response.end('OK');
+        /* [ ROUTE ] proxy to app */
+        } else {
+            //devmon_log(request);
+            var proxy_request = http.request({method:request.method,
+                                              hostname: REMOTE_ADDR,
+                                              port: REMOTE_PORT,
+                                              path: request.url,
+                                              headers: request.headers});
+            proxy_request.addListener('response', function(proxy_response) {
+                proxy_response.addListener('data', function(chunk) {
+                    response.write(chunk, 'binary');
+                });
+                proxy_response.addListener('end', function() {
+                    response.end();
+                });
+                response.writeHead(proxy_response.statusCode, proxy_response.headers);
+            });
+            request.addListener('data', function(chunk) {
+                proxy_request.write(chunk, 'binary');
+            });
+            proxy_request.addListener('error', function(err) {
+                if (err.code == 'ECONNREFUSED') {
+                    if (updatingCode) {
+                        response.writeHead(503, {'content-type': 'text/plain'});
+                        // TODO attempt to bring it back up
+                        response.end('Updating code...');
                     } else {
-                        devmon_log('Written out to '+files.code.path);
-                        updateCode(files.code.path, function(){
-                            app.kill();
-                            devmon_log('Sent SIGTERM to app, now waiting.');
-                            app.on('exit', function(){
-                                devmon_log('Spawning app.');
-                                // app is a global
-                                app = spawnApp();
-                                response.writeHead(200, {'content-type': 'text/plain'});
-                                response.end('OK');
-                                updatingCode = false;
-                            });
-                        });
-                    }
-                });
-            } else if (request.url.indexOf('__ping__') != -1) {
-                response.writeHead(200, {'content-type': 'text/plain'});
-                response.end('OK');
-            } else {
-                //devmon_log(request);
-                var proxy_request = http.request({method:request.method,
-                                                  hostname: REMOTE_ADDR,
-                                                  port: REMOTE_PORT,
-                                                  path: request.url,
-                                                  headers: request.headers});
-                proxy_request.addListener('response', function(proxy_response) {
-                    proxy_response.addListener('data', function(chunk) {
-                        response.write(chunk, 'binary');
-                    });
-                    proxy_response.addListener('end', function() {
-                        response.end();
-                    });
-                    response.writeHead(proxy_response.statusCode, proxy_response.headers);
-                });
-                request.addListener('data', function(chunk) {
-                    proxy_request.write(chunk, 'binary');
-                });
-                proxy_request.addListener('error', function(err) {
-                    if (err.code == 'ECONNREFUSED') {
                         response.writeHead(502, {'content-type': 'text/plain'});
                         // TODO attempt to bring it back up
-                        response.end('Your app is down.'); 
+                        response.end('Your app is down, try again in a few seconds. If it\'s not starting up, it might be because of a syntax error in your code, or a problem with the deployment container. Email us if the problem persists.');
                     }
-                });
-                request.addListener('end', function() {
-                    proxy_request.end();
-                });
-            }
+                }
+            });
+            request.addListener('end', function() {
+                proxy_request.end();
+            });
         }
     }).listen(LOCAL_PORT);
     return s;
@@ -140,6 +174,8 @@ var port = process.argv[2],
 
 process.chdir(cwd);
 devmon_log('Changed CWD to ' + cwd);
-// global
-app = spawnApp();
+
+spawnApp([app_cmd].concat(app_args));
+spawnNodeInspector();
+
 var proxySock = httpProxy(port, '127.0.0.1', proxyport);
